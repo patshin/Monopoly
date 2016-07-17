99cc19431e1739ddec4c5050a438c041da99fe8a
j234lu <oscarlu10000@hotmail.com>
2016-07-17 15:34:06 -0400

    Create controller.h

---
+++ controller.h
@@ -0,0 +1,37 @@
+#include "board.h"
+#include "textdisplay.h"
+#include "player.h"
+#include <string>
+
+
+
+
+class Controller
+{
+	Board *board;
+	Textdisplay *td;
+	int seed;
+	int playerindex;
+	int actionStage;
+	int NumOfPlayers;
+	bool testingMode;
+public:
+	Controller();
+	~Controller();
+	void gamestart();
+	void roll(int dice1 = 0, int dice2 = 0);
+	void next();
+	void dropout();
+	void auction();
+	void asset();
+	void all();
+	void save(string filename);
+	void load(string filename);
+	bool win();
+	void stuckAtTimline();
+	status playerstatus(); // inform the implemetation of player
+    std::string getwinner();
+	void getMode();
+	void turonTestingMode();
+	void display();
+};
