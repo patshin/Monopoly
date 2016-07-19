953277fb4ebf08f704cc8920d700f786994b97db
Patrick Sun <patricksunx@gmail.com>
2016-07-19 19:23:42 -0400

    Add files via upload

--- player.h
+++ player.h
@@ -11,33 +11,39 @@ class Player {
   int timeTim;
   int cupsOwn;
   bool gameEnd;
+  bool isBankrupted;
+  bool rollable;
   std::vector buildings;
   std::vector players;
   std::map<std::string, int> pList;
-public:  //roll, trade, finish
-  Player(char nc, vector &b, vector &p){};
+public:
+  Player(char nc, vector &b, vector &p);
   std::string getName();
   char getChar();
   int getBalance();
   int getPos();
+  bool isRollable();
   void move(int step);// setp/removep
   void goto(int p);
   void purchase(std::string bname);
   void sell(std::string bname);
-  void changeBalance(int change);
+  void changeBalance(int change);//give warning if bankrupt
   int setTimTime(int n);//0 1
   int getTimTime();
   int getCupsOwn();
   void receiveCup();
   void useCup();
-  int totalMoney();
-  void bankrupt();
+  int netCapital();
+  void bankrupt(); //delete p
   bool getGameStatus();
-  void auction(string bname, int bpos);
+  void auction(string bname, int bpos); //gai, bid or quit
   void mortgage(Property* p);
-  void notify();
   void printProperties();
-  void prop_manip(Property*, string); //mor, unmor, trade
+  void prop_manip(int ppos, int changeMoney, string s); //s = mortgage, unmortgage, tradein, tradeout, improve, unimprove
+  void isWinner();
+  bool isBankrupt();
+  void makeBankrupt();
+  bool canBankrupt(int change); //if true, give warning, cannot 付租金
   ~Player();
 };
 //improve, set status, get block, trade, win?
