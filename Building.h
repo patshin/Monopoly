14edeb72bd05025e39894fe79e9ca89a15ffccd5
sirosirom <wendysirom@gmail.com>
2016-07-21 15:18:35 -0700

    Add files via upload

--- Building.h
+++ Building.h
@@ -3,14 +3,12 @@
 #include <vector>
 #include "Player.h"
 #include "TextDisplay.h"
-#include "Board.h"
 class Building {
  bool ownable;
  std::string name;
  int site;
  std::vector <Player*> curplayer;
  BoardDisplay* display;
- Board* board;
 	public:
  Building(bool ownable, std::string name, int site, TextDisplay* board);
  int getSite();
