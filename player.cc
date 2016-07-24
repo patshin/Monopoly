bb381d88392ffd949c527553bebd0b32ccd1f31b
Patrick Sun <patricksunx@gmail.com>
2016-07-24 12:58:24 -0400

    Add files via upload

--- player.cc
+++ player.cc
@@ -10,11 +10,15 @@ using namespace std;
 Player::Player(std::string name, char nc, vector<Building*> &b, vector<Player*> &p):
   name{name},nameChar{nc},buildings{b},players{p}{
     pos = rand() % 40;
+    while(pos == 30){
+     pos = rand()%40;
+    }
     balance = 500;
+    timeTim = 0;
     cupsOwn = 0;
-    gameEnd = false;
     isBankrupted = false;
-    roll = false;
+    roll = true;
+    buildings[pos]->setPlayer(this);
 }
 
 string Player::getName(){
@@ -46,27 +50,34 @@ void Player::changeRollable(){
 }
 
 void Player::move(int step){
-  (buildings.at(pos))->removePlayer(players.at(pos));
+  buildings[pos]->removePlayer(this);  
   if (step >= 0) {
     if(pos+step<=39){
       pos += step;
     }else{
-      pos = pos+step-39;
+      pos = pos+step-40;
     }
-    (buildings.at(pos))->setPlayer(this);
   } else {
     if(pos+step<=0){
-      pos = 39+pos-step;
+      pos = 40+pos-step;
     }else{
       pos += step;
     }
-}
+  }
+  cout << "player::start setting player" << endl;
+  buildings[pos]->setPlayer(this);
+  cout << "player::finish setting" << endl;
+  buildings[pos]->method(this);
+  cout << "player::finishing this building" << endl;
 }
 
 void Player::gotoSite(int p){
-  buildings[pos]->removePlayer(players.at(pos));
+  cout << "go to " << p << endl;
+  buildings[pos]->removePlayer(this);
+  cout << "player::finish removing" << endl;
   pos = p;
   buildings[pos]->setPlayer(this);
+  cout << "Now Im in DC" << endl;
   buildings[pos]->method(this);
 }
 
@@ -164,44 +175,35 @@ int Player::netCapital(){
 
 void Player::bankrupt(){
   cout << name << " is now bankrupted! Cheers!" << endl;
-  if (players.size() <= 2){
-    gameEnd = true;
-    if(players.back() != this){
-      (players.back())->isWinner();
-    } else {
-      (players.front())->isWinner();
-    }
-  } else {
-    //restore rollupCup in tim
-    int timIndex = 10;
-    int cupRemain = buildings[timIndex]->getNumRoll();
-    buildings[timIndex]->setNumRoll(cupRemain + cupsOwn);
-    string nextCommand;
-    cout << "Please choose from the following two options:" << endl;
-    cout << "Type <auction> if you want your properties auctioned." << endl;
-    cout << "Type <give> to send your properties to the player owed." << endl;
-    try{
-      while(true){
-        cin >> nextCommand;
-        if (nextCommand == "auction"){
-          for (auto it=pList.begin(); it!=pList.end(); ++it){
-            this->auction(it->first, it->second);
-          }
-          cout << name << "'s properties were auctioned!" << endl;
-          break;
-        } else if(nextCommand == "give") {
-          Player* OwedPerson = buildings[pos]->getOwner();
-          for (auto it=pList.begin(); it!=pList.end(); ++it){
-            this->sendProperty(OwedPerson,it->second);
-          }
-          cout << "All properties were sent to " << OwedPerson << "!" << endl;
-          break;
-        } else {
-          cout << "Invalid command. Please choose again." << endl;
+  //restore rollupCup in tim
+  int timIndex = 10;
+  int cupRemain = buildings[timIndex]->getNumRoll();
+  buildings[timIndex]->setNumRoll(cupRemain + cupsOwn);
+  string nextCommand;
+  cout << "Please choose from the following two options:" << endl;
+  cout << "Type <auction> if you want your properties auctioned." << endl;
+  cout << "Type <give> to send your properties to the player owed." << endl;
+  try{
+    while(true){
+      cin >> nextCommand;
+      if (nextCommand == "auction"){
+        for (auto it=pList.begin(); it!=pList.end(); ++it){
+          this->auction(it->first, it->second);
         }
+        cout << name << "'s properties were auctioned!" << endl;
+        break;
+      } else if(nextCommand == "give") {
+        Player* OwedPerson = buildings[pos]->getOwner();
+        for (auto it=pList.begin(); it!=pList.end(); ++it){
+          this->sendProperty(OwedPerson,it->second);
+        }
+        cout << "All properties were sent to " << OwedPerson << "!" << endl;
+        break;
+      } else {
+        cout << "Invalid command. Please choose again." << endl;
       }
-    } catch (ios::failure &){}
-  }
+    }
+  } catch (ios::failure &){}
   for (auto it = players.begin() ; it != players.end(); ++it){
     if((*it)->getName() == name){
       players.erase(it);
@@ -210,85 +212,72 @@ void Player::bankrupt(){
   }
 }
 
-bool Player::getGameStatus(){
-  return gameEnd;
-}
-
 void Player::auction(string bname, int bpos){ //use map
-  if (gameEnd == false) {
-    int totalbidders = players.size() - 1;
-    int curbid = (buildings.at(pos))->getPrice();
-    int cur_index;
-    map<int, string> bidderList;
-    for(int i=0; i<totalbidders;++i){
-      if(players[i]->getName() == this->getName()){
-        continue;
-      }
-      bidderList[i] = players[i]->getName();
+  int totalbidders = players.size() - 1;
+  cout << "total bidder is " << totalbidders << endl;
+  int curbid = (buildings.at(pos))->getPrice();
+  map<int, string> bidderList;
+  for(int i=0; i<totalbidders+1;++i){
+    if(players[i]->getName() == this->getName()){
+      continue;
     }
-    cout << "Bidding started on " << bname << "!" << endl;
-    cout << "Lowest bid is " << (buildings.at(pos))->getPrice() << endl;
-    int prevPriceBid;
-    while(totalbidders >= 1) {
-      if (prevPriceBid == 0){
-        bidderList.erase(--bidderList.end());
-        prevPriceBid = 1;
+    bidderList[i] = players[i]->getName();
+  }
+  cout << "Bidding started on " << bname << "!" << endl;
+  cout << "Lowest bid is " << (buildings.at(pos))->getPrice() << endl;
+  int prevPriceBid = 1;
+  while(totalbidders >= 1) {
+    if (prevPriceBid == 0){
+      bidderList.erase(--bidderList.end());
+      prevPriceBid = 1;
+    }
+    for(auto it=bidderList.begin();it!=bidderList.end();++it){
+      if(totalbidders == 1){
+        cout << "Congrats! " << it->second << " wins the bid for ";
+        cout << bname << "!" << endl;
+        this->sendProperty(players[it->first],bpos);
+        players[it->first]->changeBalance(0 - curbid);
+        --totalbidders;
+        break;
       }
-      for(auto it=bidderList.begin();it!=bidderList.end();++it){
-        if(totalbidders == 1){
-          cout << "Congrats! " << it->second << " wins the bid for ";
-          cout << bname << "!" << endl;
-          this->sendProperty(players[it->first],bpos);
-          players[it->first]->changeBalance(0 - curbid);
-          --totalbidders;
-          break;
-        }
-        if (prevPriceBid == 0){
-          bidderList.erase(--it);
-        }
-        cout << it->second << ":" << endl;
-        cout << "Please choose from the following two options:" << endl;
-        cout << "If bidding, type in an integer greater than the current bid."
-        << endl;
-        cout << "If quiting, type 0." << endl;
-        try {
-          int priceBid;
-          cin >> priceBid;
-          if(players[it->first]->canBankrupt(0 - priceBid)) {
-            cout << "Invalid bid! You don't have enough money!" << endl;
-            --it;
-            continue;
-          }
-          if(priceBid == 0){
-            prevPriceBid = 0;
-            --totalbidders;
-            continue;
-          }else if(priceBid > curbid){
-            curbid = priceBid;
-            prevPriceBid = curbid;
-            continue;
-          }else{
-            cout << "Invalid price. Please enter your command again." << endl;
-            --it;
-            continue;
-          }
-        } catch (ios::failure &){
-          cout << "Invalid command entered." << endl;
+      if (prevPriceBid == 0){
+        bidderList.erase(--it);
+      }
+      cout << it->second << ":" << endl;
+      cout << "Please choose from the following two options:" << endl;
+      cout << "If bidding, type in an integer greater than the current bid."
+      << endl;
+      cout << "If quiting, type 0." << endl;
+      try {
+	int priceBid = 1;
+	while(cin >> priceBid){
+        if(players[it->first]->canBankrupt(0 - priceBid)) {
+          cout << "Invalid bid! You don't have enough money!" << endl;
           --it;
           continue;
         }
+        if(priceBid == 0){
+          prevPriceBid = 0;
+          --totalbidders;
+          continue;
+        }else if(priceBid > curbid){
+          curbid = priceBid;
+          prevPriceBid = curbid;
+          continue;
+        }else{
+          cout << "Invalid price. Please enter your command again." << endl;
+          --it;
+          continue;
+        }
+	}
+      } catch (ios::failure &){
+        cout << "Invalid command entered." << endl;
+        --it;
+        continue;
       }
     }
-    bidderList.clear();
   }
-}
-
-void Player::isWinner(){
-  cout << name << "wins the game!" << endl;
-  cout << "Game Over." << endl;
-//  delete players.back();
-//  delete players.front();
-//  players.clear();
+  bidderList.clear();
 }
 
 void Player::printProperties() {
@@ -296,10 +285,11 @@ void Player::printProperties() {
     cout << "You don't own any properties right now!" << endl;
     return;
   }
-  cout << "Here is the list of your properties:" << endl;
+  cout << "Here is a list of your properties:" << endl;
   for (map<string,int>::iterator it=pList.begin(); it!=pList.end(); ++it){
-    cout << (buildings[it->second])->getName() << endl;
+    cout << (buildings[it->second])->getName() << " ";
   }
+  cout << endl;
 }
 
 void Player::prop_manip(int ppos, int changeMoney, string s){
@@ -361,14 +351,14 @@ void Player::prop_manip(int ppos, int changeMoney, string s){
   } else if (s == "improve") {
     this->changeBalance(0 - buildings[ppos]->getImproveCost());
     // remember to switch improvable to false if improvement count reaches max
-    buildings[ppos]->setImproveCount(buildings[ppos]->getImproveCount() + 1);
+    buildings[ppos]->setImprovements(buildings[ppos]->getImproveCount() + 1);
   } else if (s == "unimprove") {
     if(buildings[ppos]->getImproveCount() <= 0){
       cout << "Invalid command! No more improvements to sell!" << endl;
       return;
     }
     this->changeBalance((buildings[ppos]->getImproveCost())/2);
-    buildings[ppos]->setImproveCount(buildings[ppos]->getImproveCount() - 1);
+    buildings[ppos]->setImprovements(buildings[ppos]->getImproveCount() - 1);
   } else {
     cout << "Invalid command, please type again." << endl;
   }
